cc9bdfd64c7507b0545512035f9a852c44d97539
zwj09 <1669759829@qq.com>
2021-05-24 00:16:52 +0800

    5.23

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,124 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<div>贪吃蛇</div>
+   <canvas ref="game" width="300" height="300">	</canvas>
+	 <!-- 开始游戏按钮 -->
+	 <div>
+		 <button v-if="!starting" @click="startGame">开始游戏</button>
+		 <button v-else @click="pauseGame">暂停游戏</button>
+	 </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+		const CANVAS_BORDER_COLOR = 'black';
+		const CANVAS_BACKGROUND_COLOR = 'white';
+		console.log(this.$refs.game);
+		this.game = this.$refs.game;
+		this.context = this.game.getContext('2d');
+		//画布
+		this.context.fillStyle = CANVAS_BACKGROUND_COLOR;
+		this.context.strokeStyle = CANVAS_BORDER_COLOR;
+		this.context.fillRect(0,0,this.game.width,this.game.height);
+		this.context.strokeRect(0,0,this.game.width,this.game.height);
+		this.drawSnake();
+		this.addFood();
+	},
+	data() {
+		return {
+			snake: [
+				{x: 150, y: 150},
+				{x: 140, y: 150},
+				{x: 130, y: 150},
+				{x: 120, y: 150},
+				{x: 110, y: 150},
+			],
+			game:{},
+			context:{},
+			speedX:10,
+			speedY:0,
+			food:{x:0, y:0},
+			timer:{},
+			starting:false,
+		}
+	},
+	methods: {
+		//开始游戏
+		startGame(){
+			this.starting = true;
+			this.timer = setInterval(()=>{
+				this.clearCanvas();
+				this.drawFood();
+				this.advanceSnake();
+				this.drawSnake();
+			},100)
+		},
+		//暂停游戏
+		pauseGame(){
+			this.starting = false;
+			clearInterval(this.timer);
+		},
+		//蛇身体每个部分
+		drawSnakePart(snakePart){
+			this.context.fillStyle = 'lightgreen';
+			this.context.strokeStyle = 'darkgreen';
+			this.context.fillRect(snakePart.x, snakePart.y, 10, 10);
+			this.context.strokeRect(snakePart.x, snakePart.y, 10, 10);
+		},
+		//画整条蛇
+		drawSnake(){
+			this.snake.forEach(this.drawSnakePart);
+		},
+		//让蛇动
+		advanceSnake(){
+			//新建一个头部，添加到this.snake中
+			let head = {
+				x: this.snake[0].x+this.speedX,
+				y: this.snake[0].y+this.speedY,
+				}
+				this.snake.unshift(head);
+				//通过判断是否吃到食物，选择是否取消尾部
+				let didEatFood = (this.snake[0].x === this.food.x)
+												 &&
+												 (this.snake[0].y === this.food.y);
+				if(didEatFood) {
+					//让食物随机一个位置
+					this.addFood();
+				}else{
+					this.snake.pop();
+				}
+		},
+		//更改食物的位置
+		addFood(){
+			//随机生成位置
+			this.food.x = this.randomTen(0, this.game.width - 10);
+			this.food.y = this.randomTen(0, this.game.height - 10);
+			//判断生成的位置是否在蛇身上，如果在就重新生成
+			let ifSame = this.snake.find(body=>body.x===this.food.x && body.y===this.food.y)
+			if(ifSame) this.addFood();
+		},
+		randomTen(max,min){
+			return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+		},
+		//画食物
+		drawFood(){
+			this.context.fillStyle = 'red';
+			this.context.strokeStyle = 'darkred';
+			this.context.fillRect(this.food.x, this.food.y, 10, 10);
+			this.context.strokeRect(this.food.x, this.food.y, 10, 10);
+		},
+		//清空画布
+		clearCanvas(){
+			this.context.fillStyle = 'white';
+			this.context.strokeStyle = 'black';
+			this.context.fillRect(0, 0,this.game.width,this.game.height);
+			this.context.strokeRect(0, 0,this.game.width,this.game.height);
+		},
+		up(){
+			console.log('upupup');
+		}
+	}
 }
 </script>
